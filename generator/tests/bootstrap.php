52331bdb90a853f263a3b1f5c475b8fee6a143b7
John Bain <john.bain@gmail.com>
2026-08-26 23:14:21 -0400

    test: load PHP sources before coverage starts

--- generator/tests/bootstrap.php
+++ generator/tests/bootstrap.php
@@ -3,3 +3,7 @@
 declare(strict_types=1);
 
 define('STATUS_LIGHTS_PHPUNIT', true);
+define('STATUS_LIGHTS_TESTING', true);
+define('STATUS_LIGHTS_APP_TESTING', true);
+
+require dirname(__DIR__) . '/app.php';
